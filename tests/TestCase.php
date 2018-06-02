57209639cb845f28654845d03200b7389100faec
shmurakami <shmurakami1104@gmail.com>
2018-06-02 17:01:28 +0900

    add sample test

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace shmurakami\PhraseAppSDK\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+
+}
---
+++ tests/TestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace shmurakami\PhraseAppSDK\Tests;
+
+class TestTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function t()
+    {
+        $this->assertTrue(true);
+    }
+
+}
